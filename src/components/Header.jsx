1a035b38120275f5271772e49167f516129f1951
Sudharsan-0-8 <116365632+Sudharsan-0-8@users.noreply.github.com>
2023-01-01 14:16:45 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,49 @@
-import { useState } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { useNavigate } from 'react-router-dom';
 
-import 'semantic-ui-css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Header() {
-    
-    const [activeItem, setActiveItem] = useState('');
 
-    const handleItemClick = (e, { name }) => setActiveItem(name);
+    const navigate = useNavigate();
 
     return (
-        <Menu>
-            <Menu.Item
-            name='editorials'
-            active={activeItem === 'editorials'}
-            onClick={handleItemClick}
+        <Navbar className="header" bg="light" expand="lg">
+        <Container fluid>
+            <Navbar.Brand>Social</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
             >
-                Editorials
-            </Menu.Item>
-
-            <Menu.Item
-            name='reviews'
-            active={activeItem === 'reviews'}
-            onClick={handleItemClick}
-            >
-                Reviews
-            </Menu.Item>
-
-            <Menu.Item
-            name='upcomingEvents'
-            active={activeItem === 'upcomingEvents'}
-            onClick={handleItemClick}
-            >
-                Upcoming Events
-            </Menu.Item>
-        </Menu>
+                <Nav.Link 
+                    onClick={ ()=>navigate('/') }
+                >Home</Nav.Link>
+                <Nav.Link
+                    onClick={ ()=>navigate('/login') }
+                >Login</Nav.Link>
+                <Nav.Link
+                    onClick={ ()=>navigate('/register') }
+                >Register</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+                <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
     );
 }
 
